3084ff47350349ec55f4cc80a2b6b3416ab94630
Victor Ramos <victorvilaz@gmail.com>
2024-11-26 19:48:47 -0300

    Components Part 2

--- React-main/src/App.js
+++ React-main/src/App.js
@@ -1,3 +1,20 @@
- import Menu from './components/MenuComponent'; 
- 
-    <Menu /> 
+import './App.css';
+import React, { useState } from 'react'
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+function App() {
+ const [dishes] = useState(DISHES);
+ return (
+ <div>
+ <Navbar dark color="primary" expand="md">
+ <div className="container">
+ <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+ <div>Aluno: Victor Ramos André Vila</div>
+ </div>
+ </Navbar>
+ <Menu dishes={dishes} />
+ </div>
+);
+}
+export default App;
